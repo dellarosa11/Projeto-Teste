836a0c546531b0f2dd000e5d14a4594d686302cf
Iago Mickael dos Santos <imickael2581@gmail.com>
2025-09-01 19:42:09 -0300

    Revert "Atualização"
    This reverts commit 193c8258fbffb6abde5f7e399942f3fc7b57b26a.

--- Teste.cs
+++ Teste.cs
@@ -1,4 +1,4 @@
 // See https://aka.ms/new-console-template for more information
 //Usando LINUX com Extensão do .dotnet
 using System;
-Console.WriteLine("Della gay");
+Console.WriteLine("Hello, World!");
