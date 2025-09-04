82381e1dd4f133b1c7cc4ba78f5795b8f671cf1a
dellarosa11 <matheusdellarosa11@gmail.com>
2025-09-04 20:00:25 -0300

    teste02

--- Teste.cs
+++ Teste.cs
@@ -1,4 +1,4 @@
-// See https://aka.ms/new-console-template for more information
-//Usando LINUX com Extensão do .dotnet
+
 using System;
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Projeto teste 2");
